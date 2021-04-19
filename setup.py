a854fe0a9ecd3f2881101a26be0c8e66b9b275a3
t3ddezz <75718802+t3ddezz@users.noreply.github.com>
2021-04-19 11:35:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import glob
+import os
+import pkg_resources
+
+from norse import _program
+
+setup(name='norse',
+      version='0,1'
+      scripts=['GUI/GUI.py'],
+      install_requires=[
+            "biopython>=1.70",
+            'pandas>=1.0.1',
+            "wheel>=0.34",
+            'joblib>=0.11',
+            'pysam>=0.16.0',
+            'scikit-learn==0.23.1',
+            "PuLP>=2"
+        ],
+      description='sequencing data transfer script as GUI',
+      url='https://github.com/cov-lineages/pangolin',
+      entry_points="""
+      [console_scripts]
+      {program} = norse.command:main
+      """.format(program = _program)
