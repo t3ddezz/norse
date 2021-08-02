dc75abf03be4bfb17597af8cedd2d0b89b33fa3f
Anton Graeve <T3ddezz@eduroam.med.uni-jena.de>
2021-08-02 13:37:17 +0200

    ready to merge

--- norse/norse_script.py
+++ norse/norse_script.py
@@ -23,7 +23,7 @@ program = "norse"
 file_1 = 0
 upload_sample_path = 0
 
-"""
+
 def main(sysargs = sys.argv[1:]):#main function to run script and see version
     
     parser = argparse.ArgumentParser(prog = program,
@@ -44,7 +44,7 @@ def main(sysargs = sys.argv[1:]):#main function to run script and see version
     if args.run:
         window()#function to show GUI
     
-"""    
+ 
 
 class Validator(QtGui.QValidator):#validator to restict input for flowcells,barcode and sequencinkits
     def validate(self, string, pos):
@@ -1104,5 +1104,5 @@ def window():# func to show GUI and exit correctly
     
 
 if __name__ == '__main__':#to clarify this has to be mainscript and not a importet module
-    #main()
-    window()
+    main()
+    #window()
