1be464b79fbe8640c9b66b884bf19bcc11c04c35
Anton Graeve <T3ddezz@Antons-MacBook-Pro.local>
2021-08-23 14:42:05 +0200

    cleaned up git action

--- setup.py
+++ setup.py
@@ -8,13 +8,6 @@ import pkg_resources
 setup(name='norse',
       version="0.3.0",
       scripts=['norse/norse_script.py'],
-      install_requires=[
-            "pandas==1.2.3",
-            "requests==2.25.1",
-            "paramiko==2.7.2",
-            "argparse==1.4.0",
-            "openpyxl==3.0.7",
-            "xlrd==2.0.1"],
       description='na',
       url='https://github.com/t3ddezz/norse',
       author='anton',
